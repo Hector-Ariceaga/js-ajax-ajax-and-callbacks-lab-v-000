8d833740d96d311270f46b7e7509c5cbdaab545c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-27 23:47:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-$(button).on('click', function(){
-  console.log(data)
-})
+function renderResults(data) {
+  console.log(data.items)
+  data.items.map(result => renderResult(result))
+}
 
-$.get({
-  url : 'https://api.github.com/repositories',
-  type : 'GET'
-  })
-.done(function(data) {
-  console.log(data)
-})
+function renderResult(result) {
+  console.log(result.name)
+  return 'result.name'
+}
 
 $(document).ready(function (){
-});
+  $('button').on('click', function(){
+    const searchTerms = $('#searchTerms').val()
+
+    $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
+      $('#results').append(renderResults(data))
+    })
+  })
+})
+
+
+//     {
+//       for (let i = 0; i < data.items.length; i++) {
+//         $('#results')
+//         .append(
+//           `<div><strong>${data.items[i].name}(${data.items[i].owner.login})</strong>
+//           <div><img src=${data.items[i].owner.avatar_url}></div>
+//           <p>${data.items[i].description}</p>
+//           <a href=${data.items[i].html_url}>See Full Repo Here</a><br>
+//           <a href="#" data-repo=${data.items[i].name} data-owner=${data.items[i].owner.login} onclick="showCommits(this)">Show Commits</a></div>
+//           <br><br>`)
+//       }
+//     }, "json")
+//   })
+// })
+//
+// let showCommits(el) {
+//     $.get({
+//       url : `https://api.github.com/repos/${el.dataset.owner}/${el.dataset.reoi}/commits`,
+//       type : 'GET'
+//     }).done(function(data) {
+//       console.log(data)
+//     })
+//   })
+// }
