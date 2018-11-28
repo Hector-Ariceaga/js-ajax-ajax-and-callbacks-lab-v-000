270de7593995c3ba872e6eb98d8e78acd206550d
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-28 00:44:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function renderResults(data) {
   console.log(data.items)
   data.items.map(result => renderResult(result))
-  console.log(Object.keys(data)[0])
+  console.log(Object.keys(data.items)[0])
 }
 
 function renderResult(result) {
